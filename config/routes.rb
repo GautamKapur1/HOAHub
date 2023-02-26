7aa3aa3a97fab42fb817aa7dc2c530e7a288cab6
Ajay <ajayshankar65@yahoo.com>
2023-02-26 14:59:48 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/chat", to: "home#chat"
+  get "/new", to: "home#new"
+  get "/info", to: "home#info"
+  get "/calendar", to: "home#calendar"
+  get "/form", to: "home#form"
+  get "/alert", to: "home#alert"
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/profile", to: "profile#index"
+  get "/profile/edit", to: "profile#edit"
 
 end
