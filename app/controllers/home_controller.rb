3a9249c7277439a29fcb14a57b31b372bc64752b
Ajay <ajayshankar65@yahoo.com>
2023-03-09 15:50:29 -0500

    edited home controller file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,22 @@ class HomeController < ApplicationController
   def index
     @user = User.all
   end 
+
+  def chat
+  end
+  
+  def new
+  end
+
+  def info
+  end
+  
+  def calendar
+  end
+
+  def form
+  end
+
+  def alert
+  end
 end
